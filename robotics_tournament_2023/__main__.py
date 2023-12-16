e2e67d0b3a80d3445e1d691e28a30ee51d172e55
kenny5660 <misha25_live@mail.ru>
2023-12-16 18:11:35 +0000

    fix omni

--- robotics_tournament_2023/__main__.py
+++ robotics_tournament_2023/__main__.py
@@ -1,12 +1,19 @@
-import cv2 as cv
+import cv2 
 import time
-# from ultralytics import YOLO
+import wiringpi
+from ultralytics import YOLO
 
 def main():
     wiringpi.wiringPiSetup()  
     print("Hello, World!")
     print(int(time.time()))
-    
+    img = cv2.imread('./test.jpg')
+    model = YOLO('robotics.pt')
+    results = model.predict(img, imgsz=640, conf=0.5, verbose=False)
+    if results.__len__() > 0:
+            for r in results:
+                im_array = r.plot()
+    cv2.imwrite("result.jpg",im_array)
 
 if __name__ == '__main__':
     main()
--- robotics_tournament_2023/omni_wheel.py
+++ robotics_tournament_2023/omni_wheel.py
@@ -1,6 +1,8 @@
 from robotics_tournament_2023.kangaroo_x2  import Kangaroo_x2, Kangaroo_x2_Motor
 import math
 import serial as pyserial
+import time
+
 class omni_wheel_3:
     def __init__(self,motorA,motorB,motorC,radius) -> None:
         self.motorA = motorA
@@ -22,6 +24,27 @@ class omni_wheel_3:
         self.motorB.MoveContinue(motorB_speed)
         self.motorC.MoveContinue(motorC_speed)
 
+    def move2(self,deg,speed,rot_speed):
+        deg = deg - 30
+        theta1, theta2, theta3 = 150.0, 270.0, 30.0
+        motorA_speed = math.sin((deg-theta1)*(math.pi/180.0)) 
+        motorB_speed = math.sin((deg-theta2)*(math.pi/180.0))
+        motorC_speed = math.sin((deg-theta3)*(math.pi/180.0))
+
+        motorA_speed *= speed
+        motorB_speed *= speed
+        motorC_speed *= speed
+
+        motorA_speed += self.radius*rot_speed
+        motorB_speed += self.radius*rot_speed
+        motorC_speed += self.radius*rot_speed
+
+        print(motorA_speed)
+        print(motorB_speed)
+        print(motorC_speed)
+        self.motorA.MoveContinue(motorA_speed)
+        self.motorB.MoveContinue(motorB_speed)
+        self.motorC.MoveContinue(motorC_speed)
     def stop(self):
         self.motorA.stop()
         self.motorB.stop()
@@ -29,11 +52,14 @@ class omni_wheel_3:
 
 
 if __name__ == '__main__':
-    serial = pyserial.Serial( 'COM7', 115200)
+    serial = pyserial.Serial( '/dev/ttyS2', 115200)
     kanga_130 = Kangaroo_x2(130,serial)
     kanga_135 = Kangaroo_x2(135,serial)
     motorA = Kangaroo_x2_Motor(kanga_130, '1', 3,inverted=False)
     motorB = Kangaroo_x2_Motor(kanga_130, '2', 3,inverted=True)
     motorC = Kangaroo_x2_Motor(kanga_135, '1', 3,inverted=False)
-    omni = omni_wheel_3(motorA,motorB,motorC,radius=60)#100mm
-    omni.move((360,0),0)
+    omni = omni_wheel_3(motorA,motorB,motorC,radius=1)#100mm
+    # omni.move((-360,0),0)
+    omni.move2(180,0,90)
+    time.sleep(2)
+    omni.stop()
