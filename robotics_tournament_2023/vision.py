983383bdf57f821690b242254972c205f2e48d1a
Mikhail Liamaev <32442117+kenny5660@users.noreply.github.com>
2023-12-17 06:18:04 +1000

    fix vision

--- robotics_tournament_2023/vision.py
+++ robotics_tournament_2023/vision.py
@@ -1,171 +1,108 @@
 import cv2
-import numpy as np
 from rknnlite.api import RKNNLite
-# from ultralytics.utils import ops
+import argparse
 
-OBJ_THRESH = 0.25
-NMS_THRESH = 0.45
+import cv2.dnn
+import numpy as np
 
-# The follew two param is for map test
-# OBJ_THRESH = 0.001
-# NMS_THRESH = 0.65
+from ultralytics.utils import ASSETS, yaml_load
+from ultralytics.utils.checks import check_yaml
 
-IMG_SIZE = (640, 640)  # (width, height), such as (1280, 736)
-
-CLASSES = ("BOTTLE", "RED", "WHITE")
-
-coco_id_list = [0, 1, 2]
+CLASSES = yaml_load(check_yaml('robotics2023_dataset _bottle.yaml'))['names']
+colors = np.random.uniform(0, 255, size=(len(CLASSES), 3))
 
 
-def filter_boxes(boxes, box_confidences, box_class_probs):
-    """Filter boxes with object threshold.
+def draw_bounding_box(img, class_id, confidence, x, y, x_plus_w, y_plus_h):
     """
-    box_confidences = box_confidences.reshape(-1)
-    candidate, class_num = box_class_probs.shape
+    Draws bounding boxes on the input image based on the provided arguments.
 
-    class_max_score = np.max(box_class_probs, axis=-1)
-    classes = np.argmax(box_class_probs, axis=-1)
-
-    _class_pos = np.where(class_max_score* box_confidences >= OBJ_THRESH)
-    scores = (class_max_score* box_confidences)[_class_pos]
-
-    boxes = boxes[_class_pos]
-    classes = classes[_class_pos]
-
-    return boxes, classes, scores
-
-def nms_boxes(boxes, scores):
-    """Suppress non-maximal boxes.
-    # Returns
-        keep: ndarray, index of effective boxes.
+    Args:
+        img (numpy.ndarray): The input image to draw the bounding box on.
+        class_id (int): Class ID of the detected object.
+        confidence (float): Confidence score of the detected object.
+        x (int): X-coordinate of the top-left corner of the bounding box.
+        y (int): Y-coordinate of the top-left corner of the bounding box.
+        x_plus_w (int): X-coordinate of the bottom-right corner of the bounding box.
+        y_plus_h (int): Y-coordinate of the bottom-right corner of the bounding box.
     """
-    x = boxes[:, 0]
-    y = boxes[:, 1]
-    w = boxes[:, 2] - boxes[:, 0]
-    h = boxes[:, 3] - boxes[:, 1]
-
-    areas = w * h
-    order = scores.argsort()[::-1]
-
-    keep = []
-    while order.size > 0:
-        i = order[0]
-        keep.append(i)
-
-        xx1 = np.maximum(x[i], x[order[1:]])
-        yy1 = np.maximum(y[i], y[order[1:]])
-        xx2 = np.minimum(x[i] + w[i], x[order[1:]] + w[order[1:]])
-        yy2 = np.minimum(y[i] + h[i], y[order[1:]] + h[order[1:]])
-
-        w1 = np.maximum(0.0, xx2 - xx1 + 0.00001)
-        h1 = np.maximum(0.0, yy2 - yy1 + 0.00001)
-        inter = w1 * h1
-
-        ovr = inter / (areas[i] + areas[order[1:]] - inter)
-        inds = np.where(ovr <= NMS_THRESH)[0]
-        order = order[inds + 1]
-    keep = np.array(keep)
-    return keep
-
-def dfl(position):
-    # Distribution Focal Loss (DFL)
-    import torch
-    x = torch.tensor(position)
-    n,c,h,w = x.shape
-    p_num = 4
-    mc = c//p_num
-    y = x.reshape(n,p_num,mc,h,w)
-    y = y.softmax(2)
-    acc_metrix = torch.tensor(range(mc)).float().reshape(1,1,mc,1,1)
-    y = (y*acc_metrix).sum(2)
-    return y.numpy()
+    label = f'{CLASSES[class_id]} ({confidence:.2f})'
+    color = colors[class_id]
+    cv2.rectangle(img, (x, y), (x_plus_w, y_plus_h), color, 2)
+    cv2.putText(img, label, (x - 10, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
 
 
-def box_process(position):
-    grid_h, grid_w = position.shape[2:4]
-    col, row = np.meshgrid(np.arange(0, grid_w), np.arange(0, grid_h))
-    col = col.reshape(1, 1, grid_h, grid_w)
-    row = row.reshape(1, 1, grid_h, grid_w)
-    grid = np.concatenate((col, row), axis=1)
-    stride = np.array([IMG_SIZE[1]//grid_h, IMG_SIZE[0]//grid_w]).reshape(1,2,1,1)
+def main(onnx_model, input_image):
 
-    position = dfl(position)
-    box_xy  = grid +0.5 -position[:,0:2,:,:]
-    box_xy2 = grid +0.5 +position[:,2:4,:,:]
-    xyxy = np.concatenate((box_xy*stride, box_xy2*stride), axis=1)
+    rknn_lite = RKNNLite(verbose=False, verbose_file='./inference.log')
+    sdk_version = rknn_lite.get_sdk_version()
+    print(sdk_version)
+    ret = rknn_lite.load_rknn('./robotics.rknn')
 
-    return xyxy
+    ret = rknn_lite.init_runtime(core_mask=RKNNLite.NPU_CORE_AUTO)
+    if ret != 0:
+        print('Init runtime environment failed')
+        exit(ret)
 
-def post_process(input_data):
-    boxes, scores, classes_conf = [], [], []
-    defualt_branch=3
-    pair_per_branch = len(input_data)//defualt_branch
-    # Python 忽略 score_sum 输出
-    for i in range(defualt_branch):
-        boxes.append(box_process(input_data[pair_per_branch*i]))
-        classes_conf.append(input_data[pair_per_branch*i+1])
-        scores.append(np.ones_like(input_data[pair_per_branch*i+1][:,:1,:,:], dtype=np.float32))
+    original_image: np.ndarray = cv2.imread('./test.jpg')
+    [height, width, _] = original_image.shape
 
-    def sp_flatten(_in):
-        ch = _in.shape[1]
-        _in = _in.transpose(0,2,3,1)
-        return _in.reshape(-1, ch)
+    # Prepare a square image for inference
+    length = max((height, width))
+    image = np.zeros((length, length, 3), np.uint8)
+    image[0:height, 0:width] = original_image
 
-    boxes = [sp_flatten(_v) for _v in boxes]
-    classes_conf = [sp_flatten(_v) for _v in classes_conf]
-    scores = [sp_flatten(_v) for _v in scores]
+    # Calculate scale factor
+    scale = length / 640
 
-    boxes = np.concatenate(boxes)
-    classes_conf = np.concatenate(classes_conf)
-    scores = np.concatenate(scores)
+    #resized = cv2.resize(img, IMG_SIZE, interpolation = cv2.INTER_AREA)
+    resized = np.expand_dims(image, 0)
+     # Inference
+    outputs = rknn_lite.inference(inputs=[resized])
 
-    # filter according to threshold
-    boxes, classes, scores = filter_boxes(boxes, scores, classes_conf)
+    # Prepare output array
+    outputs = np.array([cv2.transpose(outputs[0])])
+    rows = outputs.shape[1]
 
-    # nms
-    nboxes, nclasses, nscores = [], [], []
-    for c in set(classes):
-        inds = np.where(classes == c)
-        b = boxes[inds]
-        c = classes[inds]
-        s = scores[inds]
-        keep = nms_boxes(b, s)
+    boxes = []
+    scores = []
+    class_ids = []
 
-        if len(keep) != 0:
-            nboxes.append(b[keep])
-            nclasses.append(c[keep])
-            nscores.append(s[keep])
+    # Iterate through output to collect bounding boxes, confidence scores, and class IDs
+    for i in range(rows):
+        classes_scores = outputs[0][i][4:]
+        (minScore, maxScore, minClassLoc, (x, maxClassIndex)) = cv2.minMaxLoc(classes_scores)
+        if maxScore >= 0.25:
+            box = [
+                outputs[0][i][0] - (0.5 * outputs[0][i][2]), outputs[0][i][1] - (0.5 * outputs[0][i][3]),
+                outputs[0][i][2], outputs[0][i][3]]
+            boxes.append(box)
+            scores.append(maxScore)
+            class_ids.append(maxClassIndex)
 
-    if not nclasses and not nscores:
-        return None, None, None
+    # Apply NMS (Non-maximum suppression)
+    result_boxes = cv2.dnn.NMSBoxes(boxes, scores, 0.25, 0.45, 0.5)
 
-    boxes = np.concatenate(nboxes)
-    classes = np.concatenate(nclasses)
-    scores = np.concatenate(nscores)
+    detections = []
 
-    return boxes, classes, scores
+    # Iterate through NMS results to draw bounding boxes and labels
+    for i in range(len(result_boxes)):
+        index = result_boxes[i]
+        box = boxes[index]
+        detection = {
+            'class_id': class_ids[index],
+            'class_name': CLASSES[class_ids[index]],
+            'confidence': scores[index],
+            'box': box,
+            'scale': scale}
+        detections.append(detection)
+        draw_bounding_box(resized, class_ids[index], scores[index], round(box[0] * scale), round(box[1] * scale),
+                          round((box[0] + box[2]) * scale), round((box[1] + box[3]) * scale))
+
+    # Display the image with bounding boxes
+    cv2.imwrite('result.jpg', resized)
+
+    return detections
 
 
-rknn_lite = RKNNLite(verbose=False, verbose_file='./inference.log')
-sdk_version = rknn_lite.get_sdk_version()
-print(sdk_version)
-ret = rknn_lite.load_rknn('./robotics.rknn')
-
-ret = rknn_lite.init_runtime(core_mask=RKNNLite.NPU_CORE_AUTO)
-if ret != 0:
-    print('Init runtime environment failed')
-    exit(ret)
-
-img = cv2.imread('./test.jpg')
-resized = cv2.resize(img, IMG_SIZE, interpolation = cv2.INTER_AREA)
-resized = np.expand_dims(resized, 0)
- # Inference
-outputs = rknn_lite.inference(inputs=[resized])
-
-print(outputs)
-print(len(outputs))
-
-boxes, classes, scores = post_process(outputs)
-# data = ops.non_max_suppression(outputs)
-print(boxes)
+det = main(None,None)
 rknn_lite.release()
