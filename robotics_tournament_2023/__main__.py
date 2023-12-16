c053d0dfc69fce24de0b2ab36f0b81eab87767f7
kenny5660 <misha25_live@mail.ru>
2023-12-16 21:49:06 +0000

    fix detection

--- robotics_tournament_2023/__main__.py
+++ robotics_tournament_2023/__main__.py
@@ -1,19 +1,35 @@
 import cv2 
 import time
-import wiringpi
-from ultralytics import YOLO
+from  robotics_tournament_2023.vision import Vision
 
 def main():
-    wiringpi.wiringPiSetup()  
     print("Hello, World!")
-    print(int(time.time()))
-    img = cv2.imread('./test.jpg')
-    model = YOLO('robotics.pt')
-    results = model.predict(img, imgsz=640, conf=0.5, verbose=False)
-    if results.__len__() > 0:
-            for r in results:
-                im_array = r.plot()
-    cv2.imwrite("result.jpg",im_array)
+    cap = cv2.VideoCapture(0)
+    if not cap.isOpened():
+        print("Cannot open camera")
+        exit()
+    vision = Vision('./robotics_nano.rknn')
+    counter = 0
+    while True:
+        start_frame_time = time.time()
+        ret, frame = cap.read()
+        frame = cv2.flip(frame,0)
+        original = frame.copy()
+        if not ret:
+            print("Can't receive frame (stream end?). Exiting ...")
+            break
+        detections,plot_image = vision.get_detections(frame)
+        counter +=1
+        if (counter >30):
+            cv2.imwrite(f'./robot-dataset/original-{int(time.time())}.jpg', original)
+            cv2.imwrite(f'./robot-dataset/plot-{int(time.time())}.jpg', plot_image)
+            counter = 0
+        print("fps = ",1/(time.time()-start_frame_time))
+        print(detections)
+
+    cap.release()
+    vision.end()
+    
 
 if __name__ == '__main__':
     main()
--- robotics_tournament_2023/vision.py
+++ robotics_tournament_2023/vision.py
@@ -28,82 +28,86 @@ def draw_bounding_box(img, class_id, confidence, x, y, x_plus_w, y_plus_h):
     cv2.rectangle(img, (x, y), (x_plus_w, y_plus_h), color, 2)
     cv2.putText(img, label, (x - 10, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
 
+class Vision:
+    def __init__(self,model_path):
+        self.rknn_lite = RKNNLite(verbose=False, verbose_file='./inference.log')
+        ret = self.rknn_lite.load_rknn(model_path)
 
-def main(onnx_model, input_image):
+        ret = self.rknn_lite.init_runtime(core_mask=RKNNLite.NPU_CORE_AUTO)
+        if ret != 0:
+            print('Init runtime environment failed')
+            exit(ret)
 
-    model: cv2.dnn.Net = cv2.dnn.readNetFromONNX('./robotics.onnx')
-    rknn_lite = RKNNLite(verbose=False, verbose_file='./inference.log')
+    def end(self):
+        self.rknn_lite.release()
 
-    ret = rknn_lite.load_rknn('./robotics.rknn')
+    def get_detections(self,input_image):
 
-    ret = rknn_lite.init_runtime(core_mask=RKNNLite.NPU_CORE_AUTO)
-    if ret != 0:
-        print('Init runtime environment failed')
-        exit(ret)
+        original_image: np.ndarray = input_image
+        [height, width, _] = original_image.shape
 
-    original_image: np.ndarray = cv2.imread('./test.jpg')
-    [height, width, _] = original_image.shape
+        # Prepare a square image for inference
+        length = max((height, width))
+        image = np.zeros((length, length, 3), np.uint8)
+        image[0:height, 0:width] = original_image
 
-    # Prepare a square image for inference
-    length = max((height, width))
-    image = np.zeros((length, length, 3), np.uint8)
-    image[0:height, 0:width] = original_image
+        # Calculate scale factor
+        scale = length / 640
 
-    # Calculate scale factor
-    scale = length / 640
+        # resized = cv2.resize(original_image, (640,640), interpolation = cv2.INTER_AREA)
+        resized = np.expand_dims(image, 0)
 
-    # resized = cv2.resize(original_image, (640,640), interpolation = cv2.INTER_AREA)
-    resized = np.expand_dims(image, 0)
-     # Inference
-    blob = cv2.dnn.blobFromImage(image, scalefactor=1 / 255, size=(640, 640), swapRB=True)
-    model.setInput(blob)
+        outputs = self.rknn_lite.inference(inputs=[resized])
 
-    # Perform inference
-    # outputs = model.forward()
-    outputs = rknn_lite.inference(inputs=[resized])
+        # Prepare output array
+        outputs = np.array([cv2.transpose(outputs[0][0])])
+        rows = outputs.shape[1]
 
-    # Prepare output array
-    outputs = np.array([cv2.transpose(outputs[0][0])])
-    rows = outputs.shape[1]
+        boxes = []
+        scores = []
+        class_ids = []
 
-    boxes = []
-    scores = []
-    class_ids = []
+        # Iterate through output to collect bounding boxes, confidence scores, and class IDs
+        for i in range(rows):
+            classes_scores = outputs[0][i][4:]
+            (minScore, maxScore, minClassLoc, (x, maxClassIndex)) = cv2.minMaxLoc(classes_scores)
+            if maxScore >= 0.25:
+                box = [
+                    outputs[0][i][0] - (0.5 * outputs[0][i][2]), outputs[0][i][1] - (0.5 * outputs[0][i][3]),
+                    outputs[0][i][2], outputs[0][i][3]]
+                boxes.append(box)
+                scores.append(maxScore)
+                class_ids.append(maxClassIndex)
 
-    # Iterate through output to collect bounding boxes, confidence scores, and class IDs
-    for i in range(rows):
-        classes_scores = outputs[0][i][4:]
-        (minScore, maxScore, minClassLoc, (x, maxClassIndex)) = cv2.minMaxLoc(classes_scores)
-        if maxScore >= 0.25:
-            box = [
-                outputs[0][i][0] - (0.5 * outputs[0][i][2]), outputs[0][i][1] - (0.5 * outputs[0][i][3]),
-                outputs[0][i][2], outputs[0][i][3]]
-            boxes.append(box)
-            scores.append(maxScore)
-            class_ids.append(maxClassIndex)
+        # Apply NMS (Non-maximum suppression)
+        result_boxes = cv2.dnn.NMSBoxes(boxes, scores, 0.5, 0.1, 0.1)
 
-    # Apply NMS (Non-maximum suppression)
-    result_boxes = cv2.dnn.NMSBoxes(boxes, scores, 0.5, 0.1, 0.1)
+        detections = []
 
-    detections = []
+        # Iterate through NMS results to draw bounding boxes and labels
+        for i in range(len(result_boxes)):
+            index = result_boxes[i]
+            box = boxes[index]
+            detection = {
+                'class_id': class_ids[index],
+                'class_name': CLASSES[class_ids[index]],
+                'confidence': scores[index],
+                'box': box,
+                'scale': scale}
+            detections.append(detection)
+            draw_bounding_box(original_image, class_ids[index], scores[index], round(box[0] * scale), round(box[1] * scale),
+                               round((box[0] + box[2]) * scale), round((box[1] + box[3]) * scale))
+        # Display the image with bounding boxes
+        
 
-    # Iterate through NMS results to draw bounding boxes and labels
-    for i in range(len(result_boxes)):
-        index = result_boxes[i]
-        box = boxes[index]
-        detection = {
-            'class_id': class_ids[index],
-            'class_name': CLASSES[class_ids[index]],
-            'confidence': scores[index],
-            'box': box,
-            'scale': scale}
-        detections.append(detection)
-        draw_bounding_box(original_image, class_ids[index], scores[index], round(box[0] * scale), round(box[1] * scale),
-                          round((box[0] + box[2]) * scale), round((box[1] + box[3]) * scale))
-    # Display the image with bounding boxes
-    cv2.imwrite('./result.jpg', original_image)
-    rknn_lite.release()
-    return detections
+        return detections,original_image
 
+if __name__ == '__main__':
+    cap = cv2.VideoCapture(0)
+    ret, frame = cap.read()
+    frame = cv2.flip(frame,0)
+    vision = Vision('./robotics_nano.rknn')
+    detections,plot_image = vision.get_detections(frame)
+    cv2.imwrite('./result.jpg', plot_image)
+    print(detections)
 
-det = main(None,None)
